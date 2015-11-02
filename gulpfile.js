7839a4bbda6019d3d8bb8edd057c2c06827cc83f
Yuriy <ufo51@ukr.net>
2015-11-02 10:40:06 +0200

    Fixed some plugins bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,24 @@
 var gulp = require ('gulp');
-var less = require('gulp-less-sourcemap');
+var less = require('gulp-less');
+var LessPluginAutoPrefix = require('less-plugin-autoprefix');
+var sourcemaps = require('gulp-sourcemaps');
 var path = require('path');
 var fileinclude = require('gulp-file-include');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-// var mqpacker = require('css-mqpacker');
-// var csswring = require('csswring');
-
 gulp.task('default',['concat','less','fileinclude','watch']);
 
 gulp.task('less', function () {
-  gulp.src('./dev/less/style.less')
-     .pipe(less({
-         sourceMap: {
-             sourceMapRootpath: 'dev/less/style.less'
-         }
-     }))
-     .pipe(gulp.dest('./css/'));
+  var autoprefix= new LessPluginAutoPrefix({ browsers: ["last 2 versions"] });
+  return gulp.src('./dev/less/**/style.less')
+    .pipe(sourcemaps.init())
+    .pipe(less({
+      plugins: [autoprefix],
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css/'));
 });
 
 gulp.task('fileinclude', function() {
@@ -41,16 +40,4 @@ gulp.task('watch',function(){
   gulp.watch('dev/less/*.less',['less']);
   gulp.watch('dev/chunks/*.html',['fileinclude']);
   gulp.watch('dev/templates/*.html',['fileinclude']);
-  gulp.watch('css/style.css',['css']);
-});
-
-gulp.task('css', function () {
-    var processors = [
-        autoprefixer({browsers: ['last 2 version']}),
-        // mqpacker,
-        // csswring
-    ];
-    return gulp.src('./css/style.css')
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('./css'));
 });
