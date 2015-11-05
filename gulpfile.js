1c1644a515ba01cc9e8c7ad469aff96e2017a874
Yuriy <ufo51@ukr.net>
2015-11-05 10:22:09 +0200

    Bug Fixing

--- gulpfile.js
+++ gulpfile.js
@@ -14,21 +14,21 @@ gulp.task('default',['concat','less','fileinclude','connect','watch']);
 
 gulp.task('less', function () {
   var autoprefix= new LessPluginAutoPrefix({ browsers: ["last 2 versions"] });
-  return gulp.src('./dev/less/**/style.less')
-    .pipe(sourcemaps.init())
+  return gulp.src('./dev/less/style.less')
+    .pipe(sourcemaps.init('./test/'))
     .pipe(less({
       plugins: [autoprefix],
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(minifyCSS())
-    .pipe(sourcemaps.write('./'))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./css/'));
 });
 
+
 gulp.task('connect',function(){
   connect.server({
+    // root: [__dirname],
     port: 1337,
-    root: '.',
     livereload: true
   });
 })
@@ -43,17 +43,19 @@ gulp.task('fileinclude', function() {
 });
 
 gulp.task('html', function () {
-    gulp.src('./*.html')
+    gulp.src('*.html')
     .pipe(connect.reload());
 });
 gulp.task('css', function () {
-    gulp.src('./css/*.css')
+    gulp.src('css/*.css')
     .pipe(connect.reload());
 });
 
 gulp.task('concat', function() {
   return gulp.src(['./dev/js/jquery-1.11.1.min.js','./dev/js/lib/*.js'])
+
     .pipe(concat('libs.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('./js/'));
 });
 
@@ -61,6 +63,6 @@ gulp.task('watch',function(){
   gulp.watch('dev/less/*.less',['less']);
   gulp.watch('dev/chunks/*.html',['fileinclude']);
   gulp.watch('dev/templates/*.html',['fileinclude']);
-  gulp.watch(['./*.html'], ['html']);
-  gulp.watch(['./css/*.css'], ['css']);
+  gulp.watch(['*.html'], ['html']);
+  gulp.watch(['css/*.css'], ['css']);
 });
